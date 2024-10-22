6c1e2c1589b9e9da59e8cd8a86094ceb246fa168
Cagri <mucagriaktas@gmail.com>
2024-10-22 15:25:04 +0300

    added kafka-ui

--- kafka_3_8_0_KRaft_3_broker/container_images/start-kafka.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/kafka/bin/kafka-storage.sh format --config /kafka/config/server.properties --cluster-id 'EP6hyiddQNW5FPrAvR9kWw' --ignore-formatted
-
-/kafka/bin/kafka-server-start.sh /kafka/config/server.properties
